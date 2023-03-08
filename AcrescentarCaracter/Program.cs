a2ea1b2a276c4a3db838fa00c9d59d38a42699c2
Willian Marioti <willian.marioti@gmail.com>
2023-03-08 11:13:13 -0300

    Add project files.

---
+++ AcrescentarCaracter/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string nome, alterada, resultante;
+
+        nome = Console.ReadLine();
+
+
+        resultante = InsereCaracter(nome);
+
+        Console.WriteLine(resultante);
+
+        string InsereCaracter(string nome)
+        {
+            char[] aux = new char[nome.Length * 2];
+            string alterada = "";
+
+            for (int i = 0, j = 0; i < nome.Length; i++)
+            {
+                if (nome[i] != 'a' && nome[i] != 'e' && nome[i] != 'i'
+                && nome[i] != 'o' && nome[i] != 'u')
+                {
+                    aux[j + 1] = nome[i];
+                    aux[j] = '.';
+                    j = j + 2;
+                }
+                else
+                {
+                    aux[j] = nome[i];
+                    j++;
+                }
+            }
+            for (int i = 0; i < aux.Length; i++)
+            {
+                alterada = alterada + aux[i];
+            }
+            return alterada;
+        }
+    }
+}
